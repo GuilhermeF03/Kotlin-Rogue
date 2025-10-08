6a9421c8373e61bfedb13044fe6ca9fd1f792852
blurn <joao.mota.1507@gmail.com>
2025-10-08 15:19:51 +0100

    Some extra classes for the skills

--- src/main/kotlin/anchors/rogue/systems/combat/Skill.kt
+++ src/main/kotlin/anchors/rogue/systems/combat/Skill.kt
@@ -1,3 +1,53 @@
 package anchors.rogue.systems.combat
 
-data class Skill(val name: String, val description: String = "") // Expand into types of skills as needed
+data class Skill(
+    val id: UUID,
+    val name: String,
+    val description: String = "",
+    val type: SkillType,
+    val category: SkillCategory,
+    val effect: SkillEffect,
+    val basePower: Int,
+    val cost: Int
+)
+
+enum class SkillType {
+    PHYSICAL,
+    FIRE,
+    ICE,
+    LIGHTNING,
+    POISON,
+    HOLY
+}
+
+enum class SkillCategory {
+    PHYSICAL,
+    MAGIC,
+    BUFF,
+    DEBUFF
+}
+
+data class SkillEffect(
+    val effect: EffectType,
+    val multiplier: Double = 0.0,
+    val duration: Int = 0
+)
+
+enum class EffectType {
+    NONE,
+    ATTACK_UP,
+    DEFENSE_UP,
+    SPEED_UP,
+    CRIT_RATE_UP,
+    EVADE_UP,
+    ATTACK_DOWN,
+    DEFENSE_DOWN,
+    SPEED_DOWN,
+    HEAL,
+    REGENERATION,
+    POISON,
+    STUN,
+    BURN,
+    FREEZE,
+    PARALYZE,
+}
