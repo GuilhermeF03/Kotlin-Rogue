5f101005ef78ac82626268662658d24a368341c3
Pedro Carvalho <p.mgpcarvalho@gmail.com>
2025-10-08 15:01:12 +0100

    created new data for archetypes

--- src/main/kotlin/anchors/rogue/entities/Archetype.kt
+++ src/main/kotlin/anchors/rogue/entities/Archetype.kt
@@ -2,4 +2,4 @@ package anchors.rogue.entities
 
 import anchors.rogue.systems.combat.Skill
 
-data class Archetype(val name: String, val level: Int, val passive: Skill, val skillTree: List<Skill>)
+data class Archetype(val name: String, val level: Int = 0, val passive: Skill, val skillTree: List<Skill>)
