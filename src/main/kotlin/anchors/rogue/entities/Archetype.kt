bc7bc1e6d4f09fe4f05c7d0af1f8f8584bb819a5
Guilherme Ferreira <guilherme.ferreira120703@gmail.com>
2025-10-09 10:03:56 +0100

    Changed Data to data for consistency and removed src, because code already in core/src

--- src/main/kotlin/anchors/rogue/entities/Archetype.kt
+++
@@ -1,5 +0,0 @@
-package anchors.rogue.entities
-
-import anchors.rogue.systems.combat.Skill
-
-data class Archetype(val name: String, val level: Int = 0, val passive: Skill, val skillTree: List<Skill>)
--- src/main/kotlin/anchors/rogue/systems/inventory/Inventory.kt
+++
@@ -1,6 +0,0 @@
-package anchors.rogue.systems.inventory
-
-data class Inventory(
-    val equippedItems: List<Item> = listOf(),
-    val storedItems: List<Item> = listOf(),
-)
