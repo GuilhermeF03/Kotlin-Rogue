68c6b3dc1986c9299ab03d676a28e430004fa74a
Guilherme Ferreira <guilherme.ferreira120703@gmail.com>
2026-02-24 17:03:30 +0000

    New app launch

--- core/src/main/kotlin/anchors/rogue/KotlinRogue.kt
+++ core/src/main/kotlin/anchors/rogue/KotlinRogue.kt
@@ -1,27 +1,27 @@
 package anchors.rogue
 
 import anchors.rogue.screens.DemoScreen
-import io.canopy.engine.app.desktop.DesktopCanopyApp
 import io.canopy.engine.app.desktop.DesktopCanopyAppConfig
-import io.canopy.engine.core.managers.SceneManager
+import io.canopy.engine.app.desktop.desktopApp
 import io.canopy.engine.physics.systems.PhysicsSystem
 
 const val GAME_WIDTH = 1280
 const val GAME_HEIGHT = 720
 
-fun main(args: Array<String>) {
-    DesktopCanopyApp(
-        sceneManager = SceneManager {
-            registerSystem(PhysicsSystem())
-        },
-        config = DesktopCanopyAppConfig(
+fun main(args: Array<String>) = desktopApp {
+    sceneManager {
+        registerSystem(PhysicsSystem())
+    }
+
+    config(
+        DesktopCanopyAppConfig(
             title = "Kotlin Rogue",
             screenWidth = GAME_WIDTH,
             screenHeight = GAME_HEIGHT
-        ),
-        onCreate = {
-            it.addScreen(DemoScreen())
-            it.setScreen<DemoScreen>()
-        }
-    ).launch()
-}
+        )
+    )
+
+    onCreate {
+        startScreen(DemoScreen())
+    }
+}.launch()
